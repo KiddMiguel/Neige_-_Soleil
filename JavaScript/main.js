ae9c533c8441916eaf68b2a81d55efff14950039
ssamoah19 <samoahsamuel19@outlook.com>
2023-02-03 09:29:24 +0100

    update
    new chat bot by tawk

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,6 +1,7 @@
 window.onload = () => {
     ChangeImage();
     calendar();
+    tawk();
 };
 
 //Calendrier
@@ -55,3 +56,16 @@ function ChangeImage() {
         });
     });
 }
+
+function tawk() { 
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/63db87fb47425128791110a4/1go8omcfm';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+}
+tawk();
