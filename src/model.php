d9620eabdcc2269a44f5fc36cb7a59395eb15584
KiddMiguel <kidimbamiguel@gmail.com>
2023-04-14 11:19:12 +0200

    Update model.php

--- src/model.php
+++ src/model.php
@@ -35,7 +35,7 @@ class Modele
     public function insertLocataire($tab)
     {
         if ($this->unPDO != null) {
-            $request = "insert into locataire values (null, :civilite_locataire, :nom_locataire, :prenom_locataire, :email_locataire, :mdp_locataire, null, null, null, null, null, null, null)";
+            $request = "insert into locataire values (null, :civilite_locataire, :nom_locataire, :prenom_locataire, :email_locataire, :mdp_locataire, null, null, null, null, null)";
             $donnees = array(
                 ":civilite_locataire" => $tab['civilite_locataire'],
                 ":nom_locataire" => $tab['nom_locataire'],
